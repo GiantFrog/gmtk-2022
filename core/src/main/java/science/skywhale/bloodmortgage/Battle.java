1337249fd5ed4932e4b1dc1d635933ae7d4f7c76
EarthSquirrel <britneyandcaddy1@gmail.com>
2022-07-15 16:28:48 -0600

    Added initilization for battle people

--- core/src/main/java/science/skywhale/bloodmortgage/Battle.java
+++ core/src/main/java/science/skywhale/bloodmortgage/Battle.java
@@ -1,6 +1,33 @@
 package science.skywhale.bloodmortgage;
 
+import java.util.ArrayList;
+
 public class Battle {
-    private Character[] opponents;
-    private Character[] players;
+    private Character opponent;
+    private Character player;
+    private int opponentHealth;
+    private int playerHealth;
+    //private ArrayList<CharacterEntity> opponentMinions;
+    //private ArrayList<CharacterEntity> playerMinions;
+
+    public Battle(Character player, int playerStartHealth, Character opponent, int opponentStartHealth){
+        this.player = player;
+        this.playerHealth = playerStartHealth;
+        this.opponent = opponent;
+        this.opponentHealth = opponentStartHealth;
+    }
+
+
+    /*
+    // for keeping character and helath together if more than two parties
+    private class CharacterEntity {
+        public Character character;
+        public int health;
+
+        public CharacterEntity(Character character, int health){
+            this.character = character;
+            this.health = health;
+        }
+    }
+     */
 }
