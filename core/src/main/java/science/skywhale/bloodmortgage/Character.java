6f0896d2b7490cc27d342da79a9c350900852a67
EarthSquirrel <britneyandcaddy1@gmail.com>
2022-07-15 19:25:35 -0600

    Added block features

--- core/src/main/java/science/skywhale/bloodmortgage/Character.java
+++ core/src/main/java/science/skywhale/bloodmortgage/Character.java
@@ -8,10 +8,21 @@ public class Character {
     private String name;
     private Dice[] dice;
     private ArrayList<Glyph> spellbook;
+    private int battleBlock = 0;
+
 
     public Character(String name, int numDice){
         this.name = name;
         dice = new Dice[numDice];
         spellbook = new ArrayList<Glyph>();
     }
+
+    public int getBattleBlock(){
+        return battleBlock;
+    }
+
+    public void setBattleBlock(int setTo){
+        battleBlock = setTo;
+    }
+
 }
