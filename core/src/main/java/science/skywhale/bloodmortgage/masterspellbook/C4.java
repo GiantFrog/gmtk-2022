fcde017486a39b58881f8b9d06cc2da6276546c0
EarthSquirrel <britneyandcaddy1@gmail.com>
2022-07-15 19:57:48 -0600

    Init commit

---
+++ core/src/main/java/science/skywhale/bloodmortgage/masterspellbook/C4.java
@@ -0,0 +1,18 @@
+package science.skywhale.bloodmortgage.masterspellbook;
+
+import science.skywhale.bloodmortgage.Character;
+
+public class C4 extends Glyph {
+	public C4(Character owner) {
+		super();
+		this.name = "Empty";
+		this.description = "";
+		this.owner = owner;
+		this.glyphType = 0;
+	}
+	
+	@Override
+	public int useGlyph() {
+		return 0;
+	}
+}
