c7ee8aed1d3d39c938205d85005cc77b3cef38de
EarthSquirrel <britneyandcaddy1@gmail.com>
2022-07-15 18:20:58 -0600

    Added type

--- core/src/main/java/science/skywhale/bloodmortgage/masterspellbook/Common1.java
+++ core/src/main/java/science/skywhale/bloodmortgage/masterspellbook/Common1.java
@@ -1,2 +1,16 @@
-package science.skywhale.bloodmortgage.masterspellbook;public class common1 {
+package science.skywhale.bloodmortgage.masterspellbook;
+
+public class Common1 extends Glyph {
+    public Common1(Character owner) {
+        super();
+        this.name = "Common1";
+        this.description = "Increase this face by 1";
+        this.owner = owner;
+        this.glyphType = 0;
+    }
+
+    @Override
+    public int useGlyph() {
+        return this.diceSide + 1;
+    }
 }
