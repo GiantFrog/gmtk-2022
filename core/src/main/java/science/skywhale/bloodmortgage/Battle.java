023d80fc93b58b3d110107988ec946766d035c09
EarthSquirrel <britneyandcaddy1@gmail.com>
2022-07-15 19:26:28 -0600

    Check for blocking

--- core/src/main/java/science/skywhale/bloodmortgage/Battle.java
+++ core/src/main/java/science/skywhale/bloodmortgage/Battle.java
@@ -3,22 +3,21 @@ package science.skywhale.bloodmortgage;
 import java.util.ArrayList;
 
 public class Battle {
-    private Character opponent;
-    private Character player;
-    private int opponentHealth;
-    private int playerHealth;
+    private CharacterEntity opponent;
+    private CharacterEntity player;
     //private ArrayList<CharacterEntity> opponentMinions;
     //private ArrayList<CharacterEntity> playerMinions;
 
     public Battle(Character player, int playerStartHealth, Character opponent, int opponentStartHealth){
-        this.player = player;
-        this.playerHealth = playerStartHealth;
-        this.opponent = opponent;
-        this.opponentHealth = opponentStartHealth;
+        this.player = new CharacterEntity(player, playerStartHealth);
+        this.opponent = new CharacterEntity(opponent, opponentStartHealth);
+    }
+
+    public void playerTurn(){
+        // player roll dice
     }
 
 
-    /*
     // for keeping character and helath together if more than two parties
     private class CharacterEntity {
         public Character character;
@@ -28,6 +27,16 @@ public class Battle {
             this.character = character;
             this.health = health;
         }
+
+
+        public void takeDamage(int damage){
+            // if this is > 0 all damage was blocked
+            int block = character.getBattleBlock();
+            if (damage - block > 0) {
+                health = health - damage + block;
+            }
+            character.setBattleBlock(0);
+        }
     }
-     */
+
 }
