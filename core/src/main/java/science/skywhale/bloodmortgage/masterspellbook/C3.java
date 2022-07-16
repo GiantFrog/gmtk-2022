7c784d8d659e38dcfc0595a6f65cc9ea6629ea0a
EarthSquirrel <britneyandcaddy1@gmail.com>
2022-07-15 20:11:18 -0600

    Added descriptons

--- core/src/main/java/science/skywhale/bloodmortgage/masterspellbook/C3.java
+++ core/src/main/java/science/skywhale/bloodmortgage/masterspellbook/C3.java
@@ -7,7 +7,8 @@ public class C3 extends Glyph{
     public C3(Character owner) {
         super();
         this.name = "Channel Energy";
-        this.description = "";
+        this.description = "Adds a bonus to the Value of this Face. This Glyph Grows stronger each time it is ";
+		this.description += " activated, but starts with a minor penalty. Current Bonus: [placeholder for current boost]";
         this.owner = owner;
         this.glyphType = 0;
 		damage = -3;
--- core/src/main/java/science/skywhale/bloodmortgage/masterspellbook/C6.java
+++ core/src/main/java/science/skywhale/bloodmortgage/masterspellbook/C6.java
@@ -1,2 +1,19 @@
-package science.skywhale.bloodmortgage.masterspellbook;public class C6 {
+package science.skywhale.bloodmortgage.masterspellbook;
+
+import science.skywhale.bloodmortgage.Character;
+
+public class C6 extends Glyph {
+	public C6(Character owner) {
+		super();
+		this.name = "Weighted down";
+		this.description = "Decreases the likelihood that this Face will be rolled. Effect stacks each time this Glyph is Rolled.";
+		this.owner = owner;
+		this.glyphType = 0;
+	}
+	
+	@Override
+	public int useGlyph() {
+		this.weightMod--;
+		return diceSide;
+	}
 }
